3a6fbcec9acc272378752b08bd581073dd4a0597
Jarif27 <165500636+Jarif27@users.noreply.github.com>
2024-10-28 19:29:23 +0600

    Add files via upload

---
+++ CP/week1(day-1)/Prob-1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+ {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >>n;
+    
+    vector<int> cards(n);
+    for (int i = 0; i < n; ++i) 
+    {
+        cin >>cards[i];
+    }
+    
+    int serejaScore = 0, dimaScore = 0;
+    int left = 0, right = n-1;
+    
+    bool serejaTurn = true;
+    
+    while (left <= right)
+     {
+        if (cards[left] > cards[right]) 
+        {
+            if (serejaTurn) 
+            {
+                serejaScore += cards[left];
+            } 
+            else
+            {
+                dimaScore += cards[left];
+            }
+           
+            left++;
+        } 
+        else 
+        {
+            if (serejaTurn)
+            {
+                serejaScore += cards[right];
+            } 
+            else 
+            {
+                dimaScore += cards[right];
+            }
+            right--;
+        }
+        serejaTurn = !serejaTurn; 
+    }
+    
+    cout << serejaScore << " " << dimaScore << endl;
+    return 0;
+}
